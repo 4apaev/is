af2b2c67d7e02310781656615ef763ea8d6d6fff
michael <4apaev@gmail.com>
2020-09-04 02:13:52 +0300

    conf

---
+++ index.js
@@ -0,0 +1,20 @@
+import Is from "./lib/is.js"
+
+import {
+ fill,
+ echo,
+ where,
+ remove,
+} from "./lib/util.js"
+
+import claim from "./lib/claim.js"
+import define, {
+  alias
+} from "./lib/define.js"
+
+export default Is
+export {
+  define,
+  claim,
+
+}
