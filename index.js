0c555289bffd7972fb8c2d2f6b1a730efc6b83d6
michael <4apaev@gmail.com>
2020-10-02 02:59:47 +0300

    update exports

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import Is from './lib/is.js'
+import Fail from './lib/errors.js'
 import Claim from './lib/claim.js'
 import define from './lib/define.js'
 export * from './lib/util.js'
 
 export default Is
 export {
+  Fail,
   Claim,
   define,
 }
